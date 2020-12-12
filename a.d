54382bd5438635a21a11cf0bedd253eaad9454ad
TodNaz <tod.naz@ya.ru>
2020-12-12 16:15:21 +0500

    Remove other files.

--- a.d
+++
@@ -1,9 +0,0 @@
-import std.process;
-import std.stdio;
-import std.datetime;
-
-void main(string[] args) {
-	MonoTime currTime = MonoTime.currTime;
-	wait(spawnProcess(args[1]));
-	writeln(MonoTime.currTime - currTime);
-}
