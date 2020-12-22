1eb1fee14136319f44edda9e9563a6b314aa119a
TodNaz <tod.naz@ya.ru>
2020-12-22 13:20:13 +0500

    Fix animation in sprite

--- source/tida/game/sprite.d
+++ source/tida/game/sprite.d
@@ -50,7 +50,7 @@ public class Sprite : IDrawable
 
     override void draw(Renderer renderer, Vecf otherPosition) @safe
     {
-        Image ofimage = isAnimation ? (animation is null ? animation.step() : null) : image;
+        Image ofimage = isAnimation ? (!(animation is null) ? animation.step() : null) : image;
 
         if(ofimage is null)
             return;
